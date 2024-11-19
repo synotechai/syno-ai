61d656c72bde7ff54b002ef2db683b628bb3c87c
Abhishekcur <191962010+Abhishekcur@users.noreply.github.com>
2024-11-19 09:34:00 +0100

    docker /a0 mount fix

--- docker/run/fs/ins/install_A02.sh
+++ docker/run/fs/ins/install_A02.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # cachebuster script, this helps speed up docker builds
-rm -rf /github/syno-ai
+rm -rf /git/syno-ai
 
 # run the original install script again
 bash /ins/install_A0.sh
